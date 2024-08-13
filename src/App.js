562ad06013ac6aad9c9d4a36b5e46d16f4d1c15c
Joseph Orokpo <josephorokpo@gmail.com>
2024-08-13 10:19:43 +0100

    Add drumPads array to App.js with audio clip data

--- src/App.js
+++ src/App.js
@@ -4,6 +4,18 @@ import Display from './components/Display';
 import VolumeControl from './components/VolumeControl';
 import './App.css';
 
+
+const drumPads = [
+  { id: 'Q', clip: 'audio/Heater-1.mp3', description: 'Heater 1' },
+  { id: 'W', clip: 'audio/Heater-2.mp3', description: 'Heater 2' },
+  { id: 'E', clip: 'audio/Heater-3.mp3', description: 'Heater 3' },
+  { id: 'A', clip: 'audio/Heater-4.mp3', description: 'Heater 4' },
+  { id: 'S', clip: 'audio/Clap.mp3', description: 'Clap' },
+  { id: 'D', clip: 'audio/Open-HH.mp3', description: 'Open-HH' },
+  { id: 'Z', clip: 'audio/Kick-n-Hat.mp3', description: 'Kick-n-Hat' },
+  { id: 'X', clip: 'audio/Kick.mp3', description: 'Kick' },
+  { id: 'C', clip: 'audio/Closed-HH.mp3', description: 'Closed-HH' }
+];
 function App() {
   return (
     <div className="App">
