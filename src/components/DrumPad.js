e5f12f31040b6b0111b4a0682388036052302489
Joseph Orokpo <josephorokpo@gmail.com>
2024-08-15 20:44:03 +0100

    Modify DrumPad.js with correct path for audio clips

--- src/components/DrumPad.js
+++ src/components/DrumPad.js
@@ -1,12 +1,10 @@
 import React from 'react';
 
-const DrumPad = ({ id, clip, description, onClick }) => {
-  return (
-    <div className="drum-pad" id={id} onClick={() => onClick(id)}>
-      <audio className="clip" id={id} src={clip}></audio>
-      {id}
-    </div>
-  );
-};
+const DrumPad = ({ id, clip, description, onClick }) => (
+  <div className="drum-pad" id={id} onClick={() => onClick(id)}>
+    {id}
+    <audio id={id} src={process.env.PUBLIC_URL + clip} className="clip" />
+  </div>
+);
 
 export default DrumPad;
