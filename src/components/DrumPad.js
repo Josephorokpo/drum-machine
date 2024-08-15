db3b092a23294349cc523e8cefe6798a15daafa7
Joseph Orokpo <josephorokpo@gmail.com>
2024-08-15 21:32:15 +0100

    Remove unused declaration for: 'description'

--- src/components/DrumPad.js
+++ src/components/DrumPad.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const DrumPad = ({ id, clip, description, onClick }) => (
+const DrumPad = ({ id, clip, onClick }) => (
   <div className="drum-pad" id={id} onClick={() => onClick(id)}>
     {id}
     <audio id={id} src={process.env.PUBLIC_URL + clip} className="clip" />
